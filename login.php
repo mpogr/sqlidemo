08a6dcdbb7becb20c91263a39f1ccb4f6c3d162d
Michael Pogrebisky <mpogrebisky@gmail.com>
2016-12-08 09:23:40 +1100

    More formatting improvements

--- login.php
+++ login.php
@@ -1,68 +1,68 @@
 <?php
 // Make sure we use persistent cookies valid for 7 days
-ini_set ( "session.cookie_lifetime", 60 * 60 * 24 * 7 );
+ini_set("session.cookie_lifetime", 60 * 60 * 24 * 7);
 
 // Start the session
-session_start ();
+session_start();
 
 // Error messages go her
 $error = "";
 
 // On submit form, do
-if (isset ( $_POST ['submit'] )) {
-	// If any of the input fields provided is empty, display the error message
-	if (empty ( $_POST ['username'] ) || empty ( $_POST ['password'] ))
-		$error = "Username or Password is invalid";
-	else {
-		// Define $username and $password
-		$username = $_POST ['username'];
-		$password = $_POST ['password'];
-		
-		// Read the MySQL password from the file
-		$mysqlpassword = trim ( file_get_contents ( __DIR__ . "/rootpassword" ) );
-		
-		// Establish connection with the server by passing server_name, user_id and password as parameters
-		$connection = mysqli_connect ( "localhost", "root", $mysqlpassword, "mydiary_db" );
-		
-		// Use SQL prepared queries to avoid SQL injection
-		$query = mysqli_prepare ( $connection, "SELECT username FROM users WHERE password=? AND username=?" );
-		
-		// Use password and username as parameters for the prepared query
-		mysqli_stmt_bind_param ( $query, "ss", $password, $username );
-		
-		// Execute the query
-		mysqli_stmt_execute ( $query );
-		
-		// Get the username into a temporary variable
-		mysqli_stmt_bind_result ( $query, $col1 );
-		mysqli_stmt_fetch ( $query );
-		
-		// If we've got a match, store the session ID in its respecitve table
-		if ($col1 == $username) {
-			// Open the connection again (why?)
-			$connection = mysqli_connect ( "localhost", "root", $mysqlpassword, "mydiary_db" );
-			
-			// Create another prepared query
-			$query = mysqli_prepare ( $connection, "INSERT INTO sessions VALUES (?, ?)" );
-			
-			// Get the session ID
-			$sessionid = session_id ();
-			
-			// Bind the paramters to the session ID and the user name
-			mysqli_stmt_bind_param ( $query, "ss", $sessionid, $username );
-			
-			// Execute the query
-			mysqli_stmt_execute ( $query );
-			
-			// Redirect to the default page
-			header ( "location: index.php" ); // Redirecting To The Main Application Page
-		} else
-			// If something went wrong, the credentials must be invalid
-			$error = "Username or Password is invalid";
-			
-		// Close the DB connection
-		mysqli_close ( $connection );
-	}
+if (isset($_POST['submit'])) {
+    // If any of the input fields provided is empty, display the error message
+    if (empty($_POST['username']) || empty($_POST['password']))
+        $error = "Username or Password is invalid";
+    else {
+        // Define $username and $password
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        // Read the MySQL password from the file
+        $mysqlpassword = trim(file_get_contents(__DIR__ . "/rootpassword"));
+        
+        // Establish connection with the server by passing server_name, user_id and password as parameters
+        $connection = mysqli_connect("localhost", "root", $mysqlpassword, "mydiary_db");
+        
+        // Use SQL prepared queries to avoid SQL injection
+        $query = mysqli_prepare($connection, "SELECT username FROM users WHERE password=? AND username=?");
+        
+        // Use password and username as parameters for the prepared query
+        mysqli_stmt_bind_param($query, "ss", $password, $username);
+        
+        // Execute the query
+        mysqli_stmt_execute($query);
+        
+        // Get the username into a temporary variable
+        mysqli_stmt_bind_result($query, $col1);
+        mysqli_stmt_fetch($query);
+        
+        // If we've got a match, store the session ID in its respecitve table
+        if ($col1 == $username) {
+            // Open the connection again (why?)
+            $connection = mysqli_connect("localhost", "root", $mysqlpassword, "mydiary_db");
+            
+            // Create another prepared query
+            $query = mysqli_prepare($connection, "INSERT INTO sessions VALUES (?, ?)");
+            
+            // Get the session ID
+            $sessionid = session_id();
+            
+            // Bind the paramters to the session ID and the user name
+            mysqli_stmt_bind_param($query, "ss", $sessionid, $username);
+            
+            // Execute the query
+            mysqli_stmt_execute($query);
+            
+            // Redirect to the default page
+            header("location: index.php"); // Redirecting To The Main Application Page
+        } else
+            // If something went wrong, the credentials must be invalid
+            $error = "Username or Password is invalid";
+            
+            // Close the DB connection
+        mysqli_close($connection);
+    }
 }
 ?>
 
--- logout.php
+++ logout.php
@@ -1,21 +1,21 @@
 <?php
-ini_set ( "session.cookie_lifetime", 60 * 60 * 24 * 7 );
-session_start ();
+ini_set("session.cookie_lifetime", 60 * 60 * 24 * 7);
+session_start();
 
 // Read the MySQL password from a file
-$mysqlpassword = trim ( file_get_contents ( __DIR__ . "/rootpassword" ) );
+$mysqlpassword = trim(file_get_contents(__DIR__ . "/rootpassword"));
 
 // Establish connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysqli_connect ( "localhost", "root", $mysqlpassword, "mydiary_db" );
+$connection = mysqli_connect("localhost", "root", $mysqlpassword, "mydiary_db");
 
 // Delete the session ID from the DB table
-$query = mysqli_prepare ( $connection, "DELETE FROM sessions WHERE session_id=?" );
-$sessionid = session_id ();
-mysqli_stmt_bind_param ( $query, "s", $sessionid );
-mysqli_stmt_execute ( $query );
+$query = mysqli_prepare($connection, "DELETE FROM sessions WHERE session_id=?");
+$sessionid = session_id();
+mysqli_stmt_bind_param($query, "s", $sessionid);
+mysqli_stmt_execute($query);
 
-mysqli_close ( $connection ); // Closing Connection
+mysqli_close($connection); // Closing Connection
 
-if (session_destroy ()) // Destroying All Sessions
-	header ( "Location: index.php" ); // Redirecting To Home Page
+if (session_destroy()) // Destroying All Sessions
+    header("Location: index.php"); // Redirecting To Home Page
 ?>
