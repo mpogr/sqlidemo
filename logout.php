09e5cb392dab823e2ec555dd3b429b9c12770f73
mpogr <mpogrebisky@gmail.com>
2016-12-07 20:36:04 +1100

    Formatting

--- logout.php
+++ logout.php
@@ -1,18 +1,18 @@
 <?php
- ini_set("session.cookie_lifetime", 60 * 60 * 24 * 7);
- session_start();
+	ini_set("session.cookie_lifetime", 60 * 60 * 24 * 7);
+	session_start();
 
- // Read the MySQL password from a file
- $mysqlpassword = trim(file_get_contents(__DIR__ . "/rootpassword"));
+	// Read the MySQL password from a file
+	$mysqlpassword = trim(file_get_contents(__DIR__ . "/rootpassword"));
 
- // Establish connection with Server by passing server_name, user_id and password as a parameter
- $connection = mysqli_connect("localhost", "root", $mysqlpassword, "mydiary_db");
+	// Establish connection with Server by passing server_name, user_id and password as a parameter
+	$connection = mysqli_connect("localhost", "root", $mysqlpassword, "mydiary_db");
 
- // Delete the session ID from the DB table
- $ses_sql = mysqli_query($connection, "DELETE FROM sessions WHERE session_id='".session_id()."'");
+	// Delete the session ID from the DB table
+	$ses_sql = mysqli_query($connection, "DELETE FROM sessions WHERE session_id='".session_id()."'");
  
- mysqli_close($connection); // Closing Connection
+	mysqli_close($connection); // Closing Connection
 
- if(session_destroy()) // Destroying All Sessions
-    header("Location: index.php"); // Redirecting To Home Page
+	if(session_destroy()) // Destroying All Sessions
+		header("Location: index.php"); // Redirecting To Home Page
 ?>
