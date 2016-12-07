57822a06c891814eef972d79467cf30efc5bbaa6
mpogr <mpogrebisky@gmail.com>
2016-12-07 20:30:00 +1100

    Make index.php the main feed page

--- index.php
+++ index.php
@@ -1,31 +1,17 @@
 <?php
-include('login.php'); // Includes Login Script
-
-// Redirect to the profile if the session is established
-if(isset($_SESSION['login_user']))
-  header("location: profile.php");
+include('session.php');
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Login to My Secret Diary</title>
-      <link href="style.css" rel="stylesheet" type="text/css">
-  </head>
-  <body>
-    <div id="main">
-      <h1>My Secret Diary</h1>
-      <div id="login">
-        <h2>Login to My Secret Diary</h2>
-        <form action="" method="post">
-          <label>UserName :</label>
-            <input id="name" name="username" placeholder="username" type="text">
-          <label>Password :</label>
-            <input id="password" name="password" placeholder="**********" type="password">
-          <input name="submit" type="submit" value=" Login ">
-          <span><?php echo $error; ?></span>
-        </form>
-      </div>
-    </div>
-  </body>
+	<head>
+		<title>My Secret Diary Feed</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div id="profile">
+			<b id="welcome">Welcome : <i><?php echo $full_name; ?></i></b>
+			<b id="logout"><a href="logout.php">Log Out</a></b>
+		</div>
+	</body>
 </html>
