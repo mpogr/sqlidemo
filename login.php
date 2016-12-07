0ba4c216ec924f198b290b73d1ff28b0ee8b9f69
Michael Pogrebisky <mpogrebisky@gmail.com>
2016-12-07 23:54:51 +1100

    First edit in Eclispe, cosmetics

--- login.php
+++ login.php
@@ -1,10 +1,17 @@
 <?php
+	// Make sure we use persistent cookies valid for 7 days
 	ini_set("session.cookie_lifetime", 60 * 60 * 24 * 7);
-	session_start(); // Starting Session
-	$error=''; // Variable To Store Error Message
+	
+	// Start the session
+	session_start();
+	
+	// Error messages go her
+	$error = "";
 
+	// On submit form, do
 	if(isset($_POST['submit']))
 	{
+		// If any of the input fields provided is empty, display the error message
 		if(empty($_POST['username']) || empty($_POST['password']))
 			$error = "Username or Password is invalid";
 		else
@@ -13,32 +20,52 @@
 			$username=$_POST['username'];
 			$password=$_POST['password'];
 
-			// Read the MySQL password from a file
+			// Read the MySQL password from the file
 			$mysqlpassword = trim(file_get_contents(__DIR__ . "/rootpassword"));
 			
-			// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+			// Establish connection with the server by passing server_name, user_id and password as parameters
 			$connection = mysqli_connect("localhost", "root", $mysqlpassword, "mydiary_db");
 			
-			// SQL query to fetch information of registerd users and finds user match.
+			// Use SQL prepared queries to avoid SQL injection
 			$query = mysqli_prepare($connection, "SELECT username FROM users WHERE password=? AND username=?");
+			
+			// Use password and username as parameters for the prepared query
 			mysqli_stmt_bind_param($query, "ss", $password, $username);
+			
+			// Execute the query
 			mysqli_stmt_execute($query);
+			
+			// Get the username into a temporary variable
 			mysqli_stmt_bind_result($query, $col1);
 			mysqli_stmt_fetch($query);
 
+			// If we've got a match, store the session ID in its respecitve table
 			if($col1 == $username)
 			{
+				// Open the connection again (why?)
 				$connection = mysqli_connect("localhost", "root", $mysqlpassword, "mydiary_db");
+				
+				// Create another prepared query
 				$query = mysqli_prepare($connection,  "INSERT INTO sessions VALUES (?, ?)");
+				
+				// Get the session ID
 				$sessionid = session_id();
+				
+				// Bind the paramters to the session ID and the user name
 				mysqli_stmt_bind_param($query, "ss", $sessionid, $username);
+				
+				// Execute the query
 				mysqli_stmt_execute($query);
+				
+				// Redirect to the default page
 				header("location: index.php"); // Redirecting To The Main Application Page
 			}
 			else
+				// If something went wrong, the credentials must be invalid
 				$error = "Username or Password is invalid";
 			
-			mysqli_close($connection); // Closing Connection
+			// Close the DB connection
+			mysqli_close($connection);
 		}
 	}
 ?>
@@ -55,7 +82,7 @@
 			<div id="login">
 				<h2>Login to My Secret Diary</h2>
 				<form action="" method="post">
-					<label>UserName :</label>
+					<label>User Name :</label>
 					<input id="name" name="username" placeholder="username" type="text">
 					<label>Password :</label>
 					<input id="password" name="password" placeholder="**********" type="password">
